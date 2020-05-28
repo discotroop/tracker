4e1e83d2efc5214fbd84a243ee173fee2c52aba4
John Mark Goodson <john.mark.hat@gmail.com>
2020-05-28 14:46:42 -0400

    working on grayscale layout

--- src/App.js
+++ src/App.js
@@ -9,9 +9,35 @@ function App() {
   let test = TrackerLogic.tracks;
   return (
     <div className="App">
-      <div> {test[0]}</div>
-      <div> {test[1]}</div> 
-      <div> {test[2]}</div>   
+      <div className="Header">
+        <div className="Title"> <a href="#"> Title/Logo </a> </div>
+        <div className="User"> <a href="#"> User </a> </div>
+      </div>
+      <div className="ResultBox">
+        <div> {test[0]}</div>
+        <div> {test[1]}</div> 
+        <div> {test[2]}</div>   
+      </div>
+      <div className="Tracks">
+        <div className="Folder">
+          <ul>
+            <li> All </li>
+            <li> Other </li>
+            <li> Other 2 </li>
+          </ul>
+        </div>
+        <div className="TrackHolder">
+          <div className="TrackHeader">
+            <div className="TrackTitle"> Tracks </div>
+            <div className="NewTrack"> 
+              <button> New Track </button>
+            </div>
+          </div>
+          <div className="CurrentTracks">
+            here there be tracks
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
