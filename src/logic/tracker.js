58593b043cae5960dac5b9121bab12bf86fba768
John Mark Goodson <john.mark.hat@gmail.com>
2020-05-29 21:17:09 -0400

    added newTrack() and buildTrack() to tracker.js + tests

--- src/logic/tracker.js
+++ src/logic/tracker.js
@@ -1,7 +1,20 @@
 function TrackerLogic() {
     const tracks = [1, 2, 3];
+    function newTrack(trackText) {
+      return {
+        text: trackText,
+        lists: [],
+        complete: false,
+      }
+
+    }
+    function buildTrack(trackText) {
+      tracks.push(newTrack(trackText))
+    }
   return {
       tracks: tracks,
+      newTrack: newTrack,
+      buildTrack: buildTrack,
   }
 }
 
--- src/logic/tracker.test.js
+++ src/logic/tracker.test.js
@@ -6,3 +6,11 @@ test('TrackerLogic links up', () => {
     const testTrack = TrackerLogic.tracks[0];
   expect(testTrack).toBe(1);
 });
+
+
+test('Build track pushes new track to tracks', () => {
+  const logic = TrackerLogic;
+  const sampleText = 'new track';
+  logic.buildTrack(sampleText);
+  expect(logic.tracks[3].text).toBe(sampleText)
+})
